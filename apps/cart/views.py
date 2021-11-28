771d93a1baff6f7e6dbc34bd15995ee5b08c4cda
sachic <scorrear@tutanota.com>
2021-11-28 10:24:40 -0500

    Order module. Included order classes in product. Stripe connection

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,10 +1,48 @@
+import stripe
+
+from django.conf import settings
+from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from .cart import Cart
+from .forms import CheckoutForm
+
+from apps.order.utilities import checkout
 
 def cart_detail(request):
     cart = Cart(request)
 
+    if request.method == 'POST':
+        form = CheckoutForm(request.POST)
+        
+        if form.is_valid():
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            
+            stripe_token = form.cleaned_data['stripe_token']
+
+            charge = stripe.Charge.create(
+                amount = int(cart.get_total_cost() * 100),
+                currency = 'COP',
+                description = 'Cargo por MySexShop',
+                source = stripe_token
+            )
+
+            first_name = form.cleaned_data['first_name'] 
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            address = form.cleaned_data['address']
+            zipcode = form.cleaned_data['zipcode']
+            place = form.cleaned_data['place']
+
+            order = checkout(request, first_name, last_name, email, address, zipcode, place, phone, cart.get_total_cost())
+
+            cart.clear()
+
+            return redirect('success')  
+    else:
+        form = CheckoutForm()
+            
     remove_from_cart = request.GET.get('remove_from_cart', '')
     change_quantity = request.GET.get('change_quantity', '')
     quantity = request.GET.get('quantity', 0)
@@ -19,4 +57,7 @@ def cart_detail(request):
 
         return redirect('cart')
         
-    return render(request, 'cart/cart.html')
+    return render(request, 'cart/cart.html', {'form': form, 'stripe_pub_key': settings.STRIPE_PUB_KEY})
+
+def success(request):
+    return render(request, 'cart/success.html')
--- apps/order/models.py
+++ apps/order/models.py
@@ -30,4 +30,4 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.id
+        return '%s' % self.id
